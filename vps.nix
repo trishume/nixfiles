d05edcf74add2603e02d81d783a058ff3ae7564f
Tristan Hume <tris.hume@gmail.com>
2015-10-28 22:32:15 -0400

    More ZNC

--- vps.nix
+++ vps.nix
@@ -21,8 +21,14 @@
           Pass       = sha256#5da3ad0154d4c0e4d3e11ddbb62a7b6c650d95f41faec72e06c1e72cfc431915#4638!Jgyf6*d4cUm!sxU#
           <Network freenode>
             LoadModule = simple_away
-            Server     = irc.freenode.net +6667
+            Server     = irc.freenode.net +7000
             <Chan #nixos>
+            </Chan>
+            <Chan #csc>
+            </Chan>
+            <Chan #ve3uow>
+            </Chan>
+            <Chan #geekhack>
             </Chan>
           </Network>
         '';
