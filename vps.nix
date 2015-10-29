347a7a59246ab64f8a997a7f03c8a0cd3def4d59
Tristan Hume <tris.hume@gmail.com>
2015-10-28 22:28:55 -0400

    Fix ZNC 2

--- vps.nix
+++ vps.nix
@@ -14,6 +14,8 @@
       confOptions = {
         userName = "thume";
         nick = "thume";
+        useSSL = false;
+        userModules = [ "sasl"];
         # Cludge using the pass block as general user config
         passBlock = ''
           Pass       = sha256#5da3ad0154d4c0e4d3e11ddbb62a7b6c650d95f41faec72e06c1e72cfc431915#4638!Jgyf6*d4cUm!sxU#
