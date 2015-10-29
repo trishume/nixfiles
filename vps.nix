e45527c8e5c2e6ff84cd537c3b8bb57a5af25f40
Tristan Hume <tris.hume@gmail.com>
2015-10-28 22:18:02 -0400

    Fix ZNC

--- vps.nix
+++ vps.nix
@@ -11,18 +11,20 @@
       # Bug means the default dir has extra slash
       dataDir = "/var/lib/znc";
 
-      userName = "thume";
-      nick = "thume";
-      # Cludge using the pass block as general user config
-      passBlock = ''
-        Pass       = sha256#5da3ad0154d4c0e4d3e11ddbb62a7b6c650d95f41faec72e06c1e72cfc431915#4638!Jgyf6*d4cUm!sxU#
-        <Network freenode>
-          LoadModule = simple_away
-          Server     = irc.freenode.net +6667
-          <Chan #nixos>
-          </Chan>
-        </Network>
-      '';
+      confOptions = {
+        userName = "thume";
+        nick = "thume";
+        # Cludge using the pass block as general user config
+        passBlock = ''
+          Pass       = sha256#5da3ad0154d4c0e4d3e11ddbb62a7b6c650d95f41faec72e06c1e72cfc431915#4638!Jgyf6*d4cUm!sxU#
+          <Network freenode>
+            LoadModule = simple_away
+            Server     = irc.freenode.net +6667
+            <Chan #nixos>
+            </Chan>
+          </Network>
+        '';
+      };
     };
   };
   networking.firewall.allowedTCPPorts = [5000];
