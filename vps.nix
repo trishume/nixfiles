4c11a25a42600b8fd0ff9aee7619bf7113d1976e
Tristan Hume <tris.hume@gmail.com>
2015-10-28 21:59:24 -0400

    Tune ZNC

--- vps.nix
+++ vps.nix
@@ -1,10 +1,16 @@
 { config, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    znc
+   ];
+
   services = {
     znc = {
       enable = true;
       mutable = true;
+      # Bug means the default dir has extra slash
+      dataDir = "/var/lib/znc";
     };
   };
   networking.firewall.allowedTCPPorts = [5000];
