0a281fb29f8f0f53e33e98e3011f36dfe95b1956
Tristan Hume <tris.hume@gmail.com>
2016-05-02 18:46:32 -0400

    Full FRED data for dayder

--- pkgs/dayder.nix
+++ pkgs/dayder.nix
@@ -1,7 +1,12 @@
-{ stdenv, fetchFromGitHub, rustPlatform }:
+{ stdenv, fetchFromGitHub, fetchurl, rustPlatform }:
 
 with rustPlatform;
-
+let
+fredData = fetchurl {
+  url = "file:///home/tristan/misc/dayder/btsf/fred-small.btsf";
+  sha256 = "02br8f27l1rwvwq4cvcnvqvzlk4wc2zz0cy2ii9x6j7nhy63ccqz";
+};
+in
 buildRustPackage rec {
   name = "dayder-${version}";
   version = "2.0";
@@ -15,9 +20,12 @@ buildRustPackage rec {
     sha256 = "1sj97chhjwpxvchvc8kh39nald7fadpf28gmvarisxw5jhxnlyi0";
   };
 
+
+  inherit fredData;
   postFixup = ''
     mkdir -p $out/btsf
     ln -s $src/btsf/* $out/btsf/
+    ln -s $fredData $out/btsf/fred-small.btsf
     ln -s $src/public $out/public
   '';
 
